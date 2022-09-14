46635632b80ca80aa505fe7c7da5ac8aac318f61
tkattkat <48974763+tkattkat@users.noreply.github.com>
2022-09-14 10:44:24 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const password = ('samplePassword')  // replace this with desired password
+const puppeteer = require('puppeteer');
+const randopeep = require('randopeep')
+const firstName = randopeep.name({ justLast: true, prefix: false });
+const randomNum = Math.floor(Math.random() * 1000001);
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+
+    });
+
+    const page = await browser.newPage();
+    await page.goto('https://www.roblox.com/');
+    await page.waitForSelector('#MonthDropdown')
+    await page.select('#MonthDropdown', '12')
+    await page.select('#DayDropdown', '12')
+    await page.select('#YearDropdown', '2000')
+    await page.type('#signup-username', `${firstName}${randomNum}`)
+    await page.type('#signup-password', password)
+    await page.click('#MaleButton')
+    await page.waitForTimeout(2000)
+    console.log('waited')
+    await page.click('#signup-button')
+    await page.waitForNavigation()
+    await page.goto('https://www.roblox.com/chipotle')
+
+})();
